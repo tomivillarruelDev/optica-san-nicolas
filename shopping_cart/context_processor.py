34929aba0291a884af60a5bc212cbcf4c28e7121
Tomas <tomasvillarruel18@gmail.com>
2023-10-03 02:45:45 -0300

    making the site responsive

--- shopping_cart/context_processor.py
+++
@@ -1,7 +0,0 @@
-def cart_total(request):
-    total = 0
-    if 'cart' in request.session:
-        for key, value in request.session['cart'].items():
-            total += float(value['accumulated'])
-
-    return {'cart_total': total}
